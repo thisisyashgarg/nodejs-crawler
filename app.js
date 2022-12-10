f93f56cb2941cd5659bfb80622549da203c62a6c
Yash Garg <thisisyashgarg@gmail.com>
2022-12-10 12:38:09 +0530

    code cleanup, typecript code added

--- app.js
+++ app.js
@@ -7,36 +7,41 @@ const url= 'https://ibbi.gov.in/en/tender'; //url to be crawled
 const mainData = [ ] //data will be stored here
 const noOfPages = 3; // no of pages to be crawled
 
+//function to get text form the table 
+async function getPropertyFromTDS (TDS, i){
+    return (await (await TDS[i].getProperty('textContent')).jsonValue()).trim();
+}
+
 (async () =>{
     try{
         const browser = await puppeteer.launch({headless: true}); 
-        // launching the browser, if this is false, it will show the working
+        // launching the browser, if headless is false, it will show the working
         const page = await browser.newPage();
 
         //setting up your user agent
         page.setUserAgent(process.env.USER_AGENT);
-
+        console.log('crawler is running...');
 
         for (let i = 1; i <= noOfPages; i++){
             const targetURL = `${url}?page=${i}`; //dynamic url 
-            await page.goto(targetURL);
+            await page.goto(targetURL, {timeout: 0});
             await page.waitForSelector('tr'); 
 
             const TDS = await page.$$('tr td'); 
             //extracting all the targeted elements 
-            //this works as query selector all, $ - querySelector
+            //this works as query selector all
 
             for(let i = 0; i < TDS.length-2; i+=3){
                 const schema = {
-                    date: (await (await TDS[i].getProperty('textContent')).jsonValue()).trim(),
-                    subject: (await (await TDS[i+1].getProperty('textContent')).jsonValue()).trim(),
-                    contractor: (await (await TDS[i+2].getProperty('textContent')).jsonValue()).trim()
+                    date: await getPropertyFromTDS(TDS, i),
+                    subject: await getPropertyFromTDS(TDS, i+1),
+                    contractor: await getPropertyFromTDS(TDS, i+2)
                 }
             mainData.push(schema); //data being pushed in the maindata array
             }
         }
-       
-        //writing data into data.json from maindata array
+
+        //writing data into data.json from mainData array
         fs.writeFile('data/data.json', JSON.stringify(mainData, null, 4), err =>{
             if(err){
                 console.log(err)
@@ -50,6 +55,8 @@ const noOfPages = 3; // no of pages to be crawled
     }
 
     catch (err){
-        console.log(`our error: ${err}`)
+        return console.log(`our error: ${err}`)
     }
 })();
+
+
--- test.ts
+++ test.ts
@@ -1,41 +1,47 @@
 import puppeteer from 'puppeteer'
 import fs from 'fs'
-
+import dotenv from 'dotenv'
+dotenv.config();
 
 const url: string= 'https://ibbi.gov.in/en/tender'; //url to be crawled
-//data will be stored here
-const mainData: { date: string; subject: string; contractor: string }[] = []; 
+const mainData: { date: string; subject: string; contractor: string }[] = [];  //data will be stored here
 const noOfPages: number = 3; // no of pages to be crawled
+
+//function to get text form the table 
+async function getPropertyFromTDS (TDS: object, i: number){
+    return (await (await TDS[i].getProperty('textContent')).jsonValue()).trim();
+}
 
 (async () =>{
     try{
         const browser = await puppeteer.launch({headless: true}); 
-        // launching the browser, if this is false, it will show the working
+        // launching the browser, if headless is false, it will show the working
         const page = await browser.newPage();
 
         //setting up your user agent
         page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
+        console.log('crawler is running...');
 
         for (let i: number = 1; i <= noOfPages; i++){
             const targetURL: string = `${url}?page=${i}`; //dynamic url 
-            await page.goto(targetURL);
+            await page.goto(targetURL, {timeout: 0});
             await page.waitForSelector('tr'); 
 
             const TDS = await page.$$('tr td'); 
             //extracting all the targeted elements 
-            //this works as query selector all, $ - querySelector
+            //this works as query selector all
 
             for(let i: number = 0; i < TDS.length-2; i+=3){
                 const schema = {
-                    date: (await (await TDS[i].getProperty('textContent')).jsonValue()).trim(),
-                    subject: (await (await TDS[i + 1].getProperty('textContent')).jsonValue()).trim(),
-                    contractor: (await (await TDS[i + 2].getProperty('textContent')).jsonValue()).trim(),
+                    date: await getPropertyFromTDS(TDS, i),
+                    subject: await getPropertyFromTDS(TDS, i+1),
+                    contractor: await getPropertyFromTDS(TDS, i+2)
                 }
             mainData.push(schema); //data being pushed in the maindata array
             }
         }
-       
-        //writing data into data.json from maindata array
+
+        //writing data into data.json from mainData array
         fs.writeFile('data/data.json', JSON.stringify(mainData, null, 4), err =>{
             if(err){
                 console.log(err)
@@ -49,6 +55,8 @@ const noOfPages: number = 3; // no of pages to be crawled
     }
 
     catch (err){
-        console.log(`our error: ${err}`)
+        return console.log(`our error: ${err}`)
     }
 })();
+
+
